cf9ea10e94980807550ec6ba4740e982683ff158
KJS <82218506+kim12346@users.noreply.github.com>
2024-06-24 22:25:19 +0900

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,40 @@
+from tkinter import *
+
+root = Tk()
+root.title("창 제목입니당") # 제목 설정
+root.geometry("640x480") # 창 크기(가로x세로, x좌표, y좌표)
+
+def creat_new_file():
+    print("새파일을 만듭니다.")
+
+menu = Menu(root)
+
+#파일 메뉴
+menu_file = Menu(menu, tearoff=0)
+menu_file.add_command(label="New File", command=creat_new_file)
+menu_file.add_command(label="New Window")
+menu_file.add_separator()
+menu_file.add_command(label="Open File...")
+menu_file.add_separator()
+menu_file.add_command(label="Save All", state="disable") #비활성화
+menu_file.add_separator()
+menu_file.add_command(label="Exit", command=root.quit)
+menu.add_cascade(label="File", menu=menu_file)
+
+#Edit메뉴(빈값)
+menu.add_cascade(label="Edit")
+
+#Language 메뉴 추가(radio 버튼을 통해서 택1)
+menu_lang = Menu(menu, tearoff=0)
+menu_lang.add_radiobutton(label="Python")
+menu_lang.add_radiobutton(label="Java")
+menu_lang.add_radiobutton(label="C++")
+menu.add_cascade(label="Language", menu=menu_lang)
+
+# View 메뉴
+menu_view = Menu(menu, tearoff=0)
+menu_view.add_checkbutton(label="Show Minimap")
+menu.add_cascade(label="View", menu=menu_view)
+
+root.config(menu=menu)
+root.mainloop() # 창이 닫히지 않게
